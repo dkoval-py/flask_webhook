40beee216f0da1e45b9e03b2674b92045e15a053
Denys Koval <d.koval197@ukr.net>
2021-02-27 05:39:08 -0500

    Initial commit

---
+++ README.rst
@@ -0,0 +1,29 @@
+What is this?
+~~~~~~~~~~~~~
+
+| Flask webhook 
+| Flask application that cat read POST request from GitHub webhook and parce them
+
+Installation
+~~~~~~~~~~~~
+
+Flask webhook requires Python 3.x.
+
+.. code:: sh
+
+    git clone {{repo }}
+    pip install -r requirements.txt
+
+Run
+~~~
+| If you would like to run app in debug mode you should change the ``application/.env`` file.
+
+| Run using python interpreter:
+.. code:: sh
+
+    python application/webhook.py
+
+| Run using gunicorn as a deamon:
+.. code:: sh
+
+    gunicorn -b 0.0.0.0:5000 webhook:app --daemon
---
+++ application/webhook.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from dotenv import load_dotenv
+import os, json
+
+
+app = Flask(__name__)
+load_dotenv() # Initialize variables from .env file
+
+
+@app.route('/', methods=['POST'])
+def web_hook():
+    try:
+        data = json.loads(request.data)
+    except ValueError:
+        return False
+    else:
+        branch_name = data['ref'].split('/')[2]
+
+    # Block for debug regime
+    if os.getenv('ENV') == 'Dev':
+        input_file = 'input.txt'
+        try: 
+            # Write all json input data from GitHub to a file input.txt
+            with open(input_file, 'a', encoding='utf-8') as inp:
+                json.dump(data, inp)
+        except EnvironmentError:
+            with open(input_file, 'a', encoding='utf-8') as inp:
+                inp.write('Error: Write out to file failed')        
+
+    # Check from which branch webhook has been triggered
+    if branch_name == 'main' or branch_name == 'master':
+        print('This is a master branch')
+    else:    
+        print(f'THis is a {branch_name} branch')
+
+    return 'Ok'
+
+
+if __name__ == '__main__':
+    app.run(port=os.getenv('PORT'), host=os.getenv('HOST'))
